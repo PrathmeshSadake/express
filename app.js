97e3022f97553c02a83b67dbdfae701b9a6ca68a
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-08-29 20:49:44 +0530

    Created shop route and admin route

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-import express from "express";
-import bodyParser from "body-parser";
-const app = express();
-app.use(bodyParser.urlencoded());
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><input type="text" name="size" /><button type="submit">Add product</button></form>'
-  );
-});
-app.use("/product", (req, res, next) => {
-  console.log(req.body.title);
-  console.log(req.body.size);
+const express = require('express');
+const bodyParser = require('body-parser');
 
-  res.redirect("/");
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
-app.get("/", (req, res) => res.send("Hello"));
+
 app.listen(3000);
