c5bd6d3e3245b2a7c209eea34dc4bb532f16b793
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-08-29 20:43:29 +0530

    add one more input tag

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express();
 app.use(bodyParser.urlencoded());
 app.use("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add product</button></form>'
+    '<form action="/product" method="POST"><input type="text" name="title" /><input type="text" name="size" /><button type="submit">Add product</button></form>'
   );
 });
 app.use("/product", (req, res, next) => {
   console.log(req.body.title);
+  console.log(req.body.size);
+
   res.redirect("/");
 });
 app.get("/", (req, res) => res.send("Hello"));
