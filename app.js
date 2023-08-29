41428a47cddbed66a6a12df73271d2cdfb378764
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-08-29 20:41:41 +0530

    Created Add product route

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
-app.use((req, res, next) => {
-  console.log("Middleware 1");
-  next();
+app.use(bodyParser.urlencoded());
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add product</button></form>'
+  );
 });
-app.use((req, res, next) => {
-  console.log("Middleware 2");
-  next();
+app.use("/product", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect("/");
 });
+app.get("/", (req, res) => res.send("Hello"));
 app.listen(3000);
